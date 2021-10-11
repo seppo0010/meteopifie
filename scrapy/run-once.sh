6b4ed3683c59dd4df45ed6ac3deb76adb7b892f5
Sebastian Waisbrot <seppo0010@gmail.com>
2021-10-11 09:41:06 -0300

    rename meteo to scrapy

--- scrapy/run-once.sh
+++ scrapy/run-once.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -Eeux
 
-cd /meteo
+cd /scrapy
 scrapy runspider -L WARNING meteo/spiders/smn.py
--- scrapy/run.sh
+++ scrapy/run.sh
@@ -4,10 +4,10 @@ set -Eeux
 echo "[DEFAULT]
 batch_mode = on
 database = postgresql://${POSTGRES_USER}:$(cat /run/secrets/postgres-password)@database/${POSTGRES_DB}
-sources = /meteo/migrations" > /tmp/config
+sources = /scrapy/migrations" > /tmp/config
 yoyo apply --config /tmp/config
 
 while true; do
-    /meteo/run-once.sh
+    /scrapy/run-once.sh
     sleep 3600
 done
